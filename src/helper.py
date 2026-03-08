a717f941c3f42e3816e5aa317c74ab8004ef5f83
sakshigangwani <125336651+sakshigangwani@users.noreply.github.com>
2026-03-08 00:34:04 -0800

    invoked the RAG chain to get the answer

---
+++ src/helper.py
@@ -0,0 +1,48 @@
+import os
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+
+# Load PDF files
+def load_pdf(data):
+    loader = DirectoryLoader(
+        data,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
+
+    documents = loader.load()
+    return documents
+
+
+# Split documents into chunks
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20
+    )
+
+    text_chunks = text_splitter.split_documents(extracted_data)
+    return text_chunks
+
+
+# Download embedding model
+def download_hugging_face_embeddings():
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    return embeddings
+
+
+# Store embeddings in Pinecone
+def store_embeddings(text_chunks, index_name, embeddings):
+    docsearch = PineconeVectorStore.from_documents(
+        documents=text_chunks,
+        index_name=index_name,
+        embedding=embeddings
+    )
+
+    return docsearch
